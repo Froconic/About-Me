51c34ce199479418b465d9cf9291d6c2a268b1b5
froconic <froconicdesign@gmail.com>
2019-07-12 17:30:23 -0500

    added new file and edited css styles

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ alert('Hello ' + username + '! You are going to play a little game to get to kno
 //start of the loop that contains game
 for (var i = 0; i < 8; i++) {
 
-  Sott Pilgrim Question
+  // Sotty Pilgrim Question
   if (i === 0) {
     var response = prompt('Have you heard of Scott Pilgrim?');
     response = response.toLowerCase();
@@ -86,9 +86,9 @@ for (var i = 0; i < 8; i++) {
     }
   }
 
-  Years in town Question
+  // Years in town Question
   if (i === 0) {
-    var response ='';
+    var response = '';
     var answer = 7;
     var tries = 0;
     response = prompt('How long do you think I have been in Cedar Rapids. You only get 4 guesses! Hint: I\'m 25 and came here to attend college');
@@ -110,7 +110,7 @@ for (var i = 0; i < 8; i++) {
         console.log(tries + ' tries');
         correct++;
         break;
-  
+
       } else {
         alert('Err wrong');
         response = prompt('Guess again');
@@ -121,7 +121,7 @@ for (var i = 0; i < 8; i++) {
     }
   }
 
-  Programming languages Question
+  // Programming languages Question
   if (i === 6) {
     response = prompt('Guess which of the following languages I know! if you get 1 right I\'ll tell you them all! Your choices are HTML, CSS, Python, Javascript, Ruby, Perl, Go.');
     console.log(response + ' is user response');
@@ -144,62 +144,61 @@ for (var i = 0; i < 8; i++) {
       console.log(response + ' is user response');
     }
 
-    Tally of cool points
+    // Tally of cool points
     if (i === 7) {
       alert('You are have reached the end of the game! Your answers gave you a cool rating of: ' + correct);
       alert('Explore the rest of the page to check out more about me that wasn\'t included in the game!');
     }
   }
-// }
+  // }
 
 
-// }
+  // }
 
-// for (var guesses = 0; guesses < 5; guesses++) {
-//   var response = prompt('How long do you think I have been in Cedar Rapids. You only get 4 guesses! Hint: I\'m 25 and came here to attend college');
-//   console.log(response);
-//   // var answer = parseInt(response);
-//   if (response !== '7') {
-//     alert('Errrt Wrong!');
-//     response = prompt(' Guess again!');
-//     console.log(response);
-//   } else if (response === '7') {
-//     alert('Ding Ding Ding You are correct. I have lived in Cedar Rapids for 7 years');
-//     break;
-//   } else if (guesses === '4') {
-//     alert('You ran out of tries! Here is the answer anywho. I have lived in Cedar Rapids for 7 years');
-//   }
-// }
-
-
+  // for (var guesses = 0; guesses < 5; guesses++) {
+  //   var response = prompt('How long do you think I have been in Cedar Rapids. You only get 4 guesses! Hint: I\'m 25 and came here to attend college');
+  //   console.log(response);
+  //   // var answer = parseInt(response);
+  //   if (response !== '7') {
+  //     alert('Errrt Wrong!');
+  //     response = prompt(' Guess again!');
+  //     console.log(response);
+  //   } else if (response === '7') {
+  //     alert('Ding Ding Ding You are correct. I have lived in Cedar Rapids for 7 years');
+  //     break;
+  //   } else if (guesses === '4') {
+  //     alert('You ran out of tries! Here is the answer anywho. I have lived in Cedar Rapids for 7 years');
+  //   }
+  // }
 
 
-// var user;
-// user = prompt('What is your name');
-// gameStart();
-//
-// function gameStart() {
-//   for (var i = 0; i < 6; i++)
-//     if (i === 0) {
-//       var response = prompt('Did you go to college? Yes or no?');
-//       if (response.toLowerCase(response) === "yes") {
-//         alert('That is awesome ' + user + 'I did as well, although I dropped out in my third year :D');
-//       } else {
-//         alert('It\'s definitly not for everyone! I dropped out so don\'t feel too bad')
-//       }
-//
-//
-//     }
-// }
 
-// var homeTown, userName, siblings, brokenBones, renaissanceMan, diet, education, travel, questions, game;
 
-// homeTown, siblings, brokenBones, renaissanceMan, diet, education, travel = false;
+  // var user;
+  // user = prompt('What is your name');
+  // gameStart();
+  //
+  // function gameStart() {
+  //   for (var i = 0; i < 6; i++)
+  //     if (i === 0) {
+  //       var response = prompt('Did you go to college? Yes or no?');
+  //       if (response.toLowerCase(response) === "yes") {
+  //         alert('That is awesome ' + user + 'I did as well, although I dropped out in my third year :D');
+  //       } else {
+  //         alert('It\'s definitly not for everyone! I dropped out so don\'t feel too bad')
+  //       }
+  //
+  //
+  //     }
+  // }
 
-// document.getElementById('startButton').onclick = function gameStart() {
-// userName = prompt('What is your name?');
-// console.log('User name: ' + userName);
+  // var homeTown, userName, siblings, brokenBones, renaissanceMan, diet, education, travel, questions, game;
 
-// questions = prompt("")
-// };
+  // homeTown, siblings, brokenBones, renaissanceMan, diet, education, travel = false;
 
+  // document.getElementById('startButton').onclick = function gameStart() {
+  // userName = prompt('What is your name?');
+  // console.log('User name: ' + userName);
+
+  // questions = prompt("")
+  // };
